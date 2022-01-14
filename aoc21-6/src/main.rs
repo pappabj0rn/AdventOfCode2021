1a294bb4238ae37d697553cbeebd132f4e03ceeb
Greger Olofsson <greger.olofsson@gmail.com>
2022-01-14 02:50:13 +0100

    6b

--- aoc21-6/src/main.rs
+++ aoc21-6/src/main.rs
@@ -1,28 +1,56 @@
 use std::fs;
 use std::error::Error;
-use std::cmp;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string("C:\\GitHub\\AdventOfCode2021\\aoc21-6\\src\\input.txt")?;
-    let mut fish:Vec<i32> = contents
+    let fish:Vec<i32> = contents
         .split(",")
         .map(|x| x.parse::<i32>().unwrap())
         .collect();
-        
-    for _ in 0..80{        
-        for i in 0..fish.len() {
-            let mut f = fish[i]-1;
 
-            if f == -1 {
-                f = 6;
-                fish.push(8);
-            }
+    let mut f0:u64 = 0;
+    let mut f1 = 0;
+    let mut f2 = 0;
+    let mut f3 = 0;
+    let mut f4 = 0;
+    let mut f5 = 0;
+    let mut f6 = 0;
+    let mut f7 = 0;
+    let mut f8 = 0;
 
-            fish[i] = f;
+    for f in &fish {
+        match f {
+            0 => f0 += 1,
+            1 => f1 += 1,
+            2 => f2 += 1,
+            3 => f3 += 1,
+            4 => f4 += 1,
+            5 => f5 += 1,
+            6 => f6 += 1,
+            7 => f7 += 1,
+            8 => f8 += 1,
+            _ => panic!("ftw?"),
         }
     }
+        
+    println!("gen: 0");
 
-    println!("fish: {}", fish.len());
+    for n in 0..256{
+        let spawners = f0;
+
+        f0 = f1;
+        f1 = f2;
+        f2 = f3;
+        f3 = f4;
+        f4 = f5;
+        f5 = f6;
+        f6 = f7+spawners;
+        f7 = f8;
+        f8 = spawners;
+        println!("gen: {}",n+1);
+    }
+
+    println!("fish: {}", f0+f1+f2+f3+f4+f5+f6+f7+f8);
     
     Ok(())
 }
